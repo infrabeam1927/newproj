1a6698e004f8ea6ca0098f521b31da9e0f8786bc
Aaditya Senthilkumar <75241997+infrabeam1927@users.noreply.github.com>
2024-01-19 07:46:36 +0000

    API commit 2

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ let getJoke=()=>{
 }
 btn.addEventListener("click",getJoke)
 getJoke();
+const quotecontainer = document.getElementById("quote")
+const btn1 = document.getElementById("btn1")
+const url2 = 'https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '53aac64b09mshcc81f7a23f2a4a8p181155jsn5800e4f73360',
+		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+	}
+};
+
+let getQuote=()=>{
+    fetch(url2,options)
+    .then(data=>data.json())
+    .then(item=>{
+        quotecontainer.textContent=`${item.text}`
+    })
+
+}
+
+btn1.addEventListener("click",getQuote)
+getQuote()
